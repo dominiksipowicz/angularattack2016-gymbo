73df7ae9662cda0e8ac4b06de28dcbe24ddc1e4a
Marian Zburlea <marianzburlea@gmail.com>
2016-05-14 12:11:34 +0100

    Add user info component. It looks NICE!

---
+++ src/app/components/user-info/user-info.ts
@@ -0,0 +1,32 @@
+/**
+ * Created by Marian on 14/05/2016.
+ */
+import { Component } from '@angular/core';
+import {AuthService} from "../../common/services/auth.service";
+
+@Component({
+  selector: 'user-info',
+  providers: [AuthService],
+  template: `
+<div class="user-info">
+
+  <div class="user-data-cell"><img [src]="auth.avatar" alt="Avatar"></div>
+  <div class="user-data-cell">{{ auth.displayName }}</div>
+</div>
+  `,
+  styles: [`
+  img {
+    width: 40px;
+  }
+  .user-data-cell {
+    padding: 5px;
+  }
+  .user-info {
+    display: flex;
+    align-items: center;
+  }
+  `]
+})
+export class UserInfo {
+  constructor(private auth: AuthService) {}
+}
--- src/app/gymbo-app.ts
+++ src/app/gymbo-app.ts
@@ -8,12 +8,13 @@ import {WorkoutWall} from './components/workout-wall/workout-wall';
 import {SignIn} from './components/sign-in/sign-in';
 import {SignOut} from './components/sign-out/sign-out';
 import {AuthService} from "./common/services/auth.service";
+import {UserInfo} from './components/user-info/user-info';
 
 @Component({
   selector: 'gymbo-app',
   providers: [AuthService],
   pipes: [],
-  directives: [ROUTER_DIRECTIVES, SignIn, SignOut],
+  directives: [ROUTER_DIRECTIVES, SignIn, SignOut, UserInfo],
   templateUrl: 'app/gymbo-app.html',
 })
 @RouteConfig([
