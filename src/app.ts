f5933497e7ac4c262c3b0df50cd7e89453e88283
Marian Zburlea <marianzburlea@gmail.com>
2016-05-14 10:56:00 +0100

    Add FireBase providers to app root

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import {bootstrap} from '@angular/platform-browser-dynamic';
 import {provide, enableProdMode} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {FIREBASE_PROVIDERS, defaultFirebase, firebaseAuthConfig, AuthMethods} from 'angularfire2';
 
 import {GymboApp} from './app/gymbo-app';
 
@@ -12,6 +13,10 @@ import {GymboApp} from './app/gymbo-app';
 bootstrap(GymboApp, [
   HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
-  provide(LocationStrategy, {useClass: PathLocationStrategy})
+  provide(LocationStrategy, {useClass: PathLocationStrategy}),
+  // Add FireBase providers
+  defaultFirebase('https://bitbee.firebaseio.com'),
+  firebaseAuthConfig({method: AuthMethods.Popup}),
+  FIREBASE_PROVIDERS
 ])
 .catch(err => console.error(err));
