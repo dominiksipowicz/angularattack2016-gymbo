7ac5ccd13e1cbf4d1c86f4b321193b2c954299e1
Przemek <przemyslaw.kalka@gmail.com>
2016-05-14 11:33:37 +0100

    Dynamic Form for Add Workout Base

--- src/app/components/add-workout/add-workout.ts
+++ src/app/components/add-workout/add-workout.ts
@@ -1,19 +1,34 @@
 import {Component} from '@angular/core';
-import {Http} from '@angular/http';
-
+import { DynamicForm } from '../dynamic-form/dynamic-form.component';
+import { TextboxQuestion }  from '../dynamic-form/questions/textbox-question';
 
 @Component({
   selector: 'about',
   templateUrl: 'app/components/add-workout/add-workout.html',
   styleUrls: ['app/components/add-workout/add-workout.css'],
   providers: [],
-  directives: [],
+  directives: [DynamicForm],
   pipes: []
 })
 export class AddWorkout {
+  questions:any[];
 
-  constructor(http: Http) {
-
+  constructor() {
+    this.questions = [
+      new TextboxQuestion({
+        key:'firstName',
+        label:'First name',
+        value:'Bombasto',
+        required: true,
+        order: 1
+      }),
+      new TextboxQuestion({
+        key:'emailAddress',
+        label:'Email',
+        type: 'email',
+        order: 2
+      })
+    ]
   }
 
   ngOnInit() {
