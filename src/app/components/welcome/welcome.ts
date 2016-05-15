cab6562fa2c3da4d1fe4cbe54c914a3df081964a
Dominik Sipowicz <dominik.sipowicz@gmail.com>
2016-05-15 11:19:04 +0100

    some styling

--- src/app/components/welcome/welcome.ts
+++ src/app/components/welcome/welcome.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
+import {Router, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 
 @Component({
   selector: 'home',
   templateUrl: 'app/components/welcome/welcome.html',
   styleUrls: ['app/components/welcome/welcome.css'],
   providers: [],
-  directives: [],
+  directives: [ROUTER_DIRECTIVES],
   pipes: []
 })
 export class Welcome {
