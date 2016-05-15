e7b1e9e1f277f77a1c76207b4964288630937378
Dominik Sipowicz <dominik.sipowicz@gmail.com>
2016-05-15 15:16:23 +0100

    welcome copy

--- src/app/components/welcome/welcome.ts
+++ src/app/components/welcome/welcome.ts
@@ -1,17 +1,19 @@
 import {Component} from '@angular/core';
 import {Router, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {AuthService} from '../../common/services/auth.service';
+import {SignIn} from '../sign-in/sign-in';
 
 @Component({
   selector: 'home',
   templateUrl: 'app/components/welcome/welcome.html',
   styleUrls: ['app/components/welcome/welcome.css'],
-  providers: [],
-  directives: [ROUTER_DIRECTIVES],
+  providers: [AuthService],
+  directives: [ROUTER_DIRECTIVES, SignIn],
   pipes: []
 })
 export class Welcome {
 
-  constructor() {}
+  constructor(private auth: AuthService) {}
 
   ngOnInit() {
 
